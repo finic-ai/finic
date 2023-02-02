06d31170efae08c87f41357dd5abdc1943f9f1a2
Jason Fan <jason.wc.fan@gmail.com>
2023-02-02 10:33:05 -0800

    moved some files around, added chrome extension

---
+++ chrome-extension/src/index.js
@@ -0,0 +1,14 @@
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ColorModeScript />
+    <App />
+  </StrictMode>
+);
