571c57ac05ac82ea6004eb6177cdc3c58eee5ee9
Jason Fan <jason.wc.fan@gmail.com>
2023-02-03 15:45:21 -0800

    fixed some styling issues

--- chrome-extension/buff-chat-widget/src/index.tsx
+++ chrome-extension/buff-chat-widget/src/index.tsx
@@ -8,14 +8,7 @@ rootElement.id = "react-chrome-app";
 const globalStyles = document.createElement("style");
 globalStyles.innerHTML = `
   #${rootElement.id} {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 300px;
-  height: 100vh;
-  background: #ffffff;
-  border-right: 1px solid #c2c2c2;
-  z-index: 999;
+  all: unset;
   }
 `;
 
