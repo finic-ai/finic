d416cc449737f7654f9edb26b94c1852a8b0a570
Ayan Bandyopadhyay <ayanb9440@gmail.com>
2023-05-03 17:10:55 -0700

    Update sidekick frontend client library

--- sidekick-demo/src/App.tsx
+++ sidekick-demo/src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Button,
   Modal,
 } from "flowbite-react";
 import './App.css';
-import Sidekick from "getsidekick"
-import SidekickModal from 'getsidekick';
+import {useNotionOAuth} from "getsidekick"
 
 const App: React.FC = () => {
 
+  const publicKey = "5c362fda-bb72-4a6c-b930-71f33ad45f79"
+  const connectionId = "2cbaa840-0b21-4d8e-924c-e418a08ce53f"
+
+  const { authorize, authCode } = useNotionOAuth("notion", connectionId, publicKey)
+
+  useEffect(() => {
+    authorize()
+  }, [])
+
   return (
     <div className="App">
-     <SidekickModal />
+     {/* <MyComponent text={'hello'} /> */}
     </div>
   );
 }
