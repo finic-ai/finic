acc56ae96aa5f4fe45e347e5cc9fb8f9ebb67a56
Ayan Bandyopadhyay <ayanb9440@gmail.com>
2023-05-04 22:42:51 -0700

    Kick off new vercel deployment

--- sidekick-server/connectors/connector_utils.py
+++ sidekick-server/connectors/connector_utils.py
@@ -3,6 +3,6 @@ from models.models import AppConfig, DataConnector, ConnectorId
 from typing import Optional
 
 def get_connector_for_id(connector_id: ConnectorId, config: AppConfig) -> Optional[DataConnector]:
-    if connector_id == 'notion':
+    if connector_id == 'notion': 
         return NotionConnector(config)
     return None
