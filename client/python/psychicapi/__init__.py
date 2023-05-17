dc6a6f402182e5b8c751c8865bdee37b688f0af6
Ayan Bandyopadhyay <ayanb9440@gmail.com>
2023-05-17 12:30:41 -0700

    Update name of the package and expose the connector id enum

---
+++ client/python/psychicapi/__init__.py
@@ -0,0 +1,1 @@
+from .psychic import Psychic, ConnectorId
--- client/python/setup.py
+++ client/python/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='psychic-link',
-    version='0.1',
+    name='psychicapi',
+    version='0.2',
     description='Psychic.dev is an open-source universal data connector for knowledgebases.',
     author='Ayan Bandyopadhyay',
     author_email='ayan@getsidekick.ai',
-    packages=['psychic-link'],
+    packages=['psychicapi'],
     install_requires=[
         'requests',
     ],
