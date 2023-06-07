561451de53ef58058164f6ca0193021192e8ae8c
Jason Fan <jason.wc.fan@gmail.com>
2023-06-06 22:20:50 -0400

    fix bug with connections table timestamp query

--- server/appstatestore/statestore.py
+++ server/appstatestore/statestore.py
@@ -95,7 +95,7 @@ class StateStore:
 
         connections: List[Connection] = []
         # Sort connections by when they were created
-        for row in sorted(response.data, key=lambda x: parse(x['timestamp']), reverse=True):
+        for row in sorted(response.data, key=lambda x: parse(x['created_at']), reverse=True):
             connections.append(
                 Connection(
                     account_id=row['account_id'],
