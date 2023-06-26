04ea95963ef81757711df7e22ff92cf01aba5911
Ayan Bandyopadhyay <ayanb9440@gmail.com>
2023-06-25 22:12:47 -0700

    Add option to limit visible connectors

--- dashboard/src/pages/settings.tsx
+++ dashboard/src/pages/settings.tsx
@@ -5,6 +5,7 @@ import {
   Label,
   Spinner,
   TextInput,
+  Checkbox,
 } from "flowbite-react";
 import { FC, useEffect } from "react";
 import { useState } from "react";
@@ -56,11 +57,26 @@ const ProductsTable: FC = function () {
 
   const [name, setName] = useState('')
   const [logoUrl, setLogoUrl] = useState('')
+  const [enabledConnectors, setEnabledConnectors] = useState<string[]>([])
   const [loading, setLoading] = useState(true)
   const [saved, setSaved] = useState(false)
   const [webhookUrl, setWebhookUrl] = useState('')
   const [webhookSaved, setWebhookSaved] = useState(false)
   const [webhookLoading, setWebhookLoading] = useState(true)
+
+  const allConnectors = [
+    { id: 'confluence', name: 'Confluence' },
+    { id: 'slack', name: 'Slack' },
+    { id: 'dropbox', name: 'Dropbox' },
+    { id: 'zendesk', name: 'Zendesk' },
+    { id: 'intercom', name: 'Intercom' },
+    { id: 'hubspot', name: 'Hubspot' },
+    { id: 'notion', name: 'Notion' },
+    { id: 'readme', name: 'Readme.com' },
+    { id: 'salesforce', name: 'Salesforce' },
+    { id: 'gdrive', name: 'Google Drive' },
+    { id: 'github', name: 'Github' },
+  ]
 
 
   useEffect(() => {
@@ -73,6 +89,12 @@ const ProductsTable: FC = function () {
           if (data && data[0]) {
               setName(data[0].name)
               setLogoUrl(data[0].logo)
+              if (data[0].enabled_connectors !== null) {
+                  setEnabledConnectors(data[0].enabled_connectors)
+              } else {
+                  // All connector IDs are enabled by default
+                  setEnabledConnectors(allConnectors.map(connector => connector.id))
+              }
           }
           setLoading(false)
       }
@@ -111,7 +133,7 @@ const ProductsTable: FC = function () {
       }
       const { data, error } = await supabase
       .from('settings')
-      .upsert({ name, logo: downloadUrl, app_id: appId })
+      .upsert({ name, logo: downloadUrl, app_id: appId, enabled_connectors: enabledConnectors })
       .eq('app_id', appId)
       .select()
       setLoading(false)
@@ -136,6 +158,27 @@ const ProductsTable: FC = function () {
         console.log(data)
         setWebhookSaved(true)
     }
+  }
+
+  const handleCheckChange = (e: any) => {
+    if (e.target.checked) {
+      // append value to enabledConnectors if it's not already included
+      if (!enabledConnectors.includes(e.target.value)) {
+        setEnabledConnectors([...enabledConnectors, String(e.target.value)]);
+      }
+    } else {
+      // remove value from enabledConnectors when unchecked
+      setEnabledConnectors(enabledConnectors.filter(item => item !== e.target.value));
+    }
+  };
+
+  const ConnectorCheckbox = ({ connectorId, connectorName }: { connectorId: string, connectorName: string }) => {
+    return (
+      <div className="flex flex-row mt-4">
+        <Checkbox value={connectorId} checked={enabledConnectors.includes(connectorId)} onChange={handleCheckChange} />
+        <Label htmlFor="apiKeys.newKey" className="ml-2">{connectorName}</Label>
+      </div>
+    )
   }
 
 
@@ -167,6 +210,12 @@ const ProductsTable: FC = function () {
       onChange={(e) => setLogoUrl(e.target.value)}
       helperText="URL to your company logo. This will be displayed in the Psychic link modal. Must be a google drive link."
     />
+  </div>
+  <div>
+    <Label htmlFor="apiKeys.newKey">Enabled Connectors</Label>
+    {allConnectors.map(connector => (
+      <ConnectorCheckbox connectorId={connector.id} connectorName={connector.name} />
+    ))}
   </div>
   <div className="flex justify-beginning">
       <Button color="primary" onClick={updateSettings}>
